f15363dcad7d61698d78e2521059bf9f17dcbd0c
CC-Student <CC-Student@MININT-7NB99MC>
2020-11-04 09:46:57 +0100

    Datentyp changes

--- Datentypen/Program.cs
+++ Datentypen/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Datentypen
 {
@@ -6,6 +7,8 @@ namespace Datentypen
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("asdas");
+
             long Ganzzahlen64Bit; // -9 trillionen bis 9 trillionen
             ulong Ganzzahlen64BitPositiv; // 0 bis 18 trillionen
 
@@ -17,8 +20,8 @@ namespace Datentypen
             short Ganzzahl16Bit; // -32 tausend bis 32 tausend
             ushort Ganzzah16BitPositiv; // 0 bis 65 tausend
 
-            sbyte Ganzzahl18Bit; // -128 bis 127
-            byte Ganzzahl18BitPositiv; // 0 bis 255
+            sbyte Ganzzahl8Bit; // -128 bis 127
+            byte Ganzzahl8BitPositiv; // 0 bis 255
 
             /////////////////////////////////
 
@@ -29,6 +32,35 @@ namespace Datentypen
             ///////////////////////////////////
 
             bool JaOderNein; // exakt zwei wertr. true(1) und false (0)
+
+            //////////////////////////////////
+
+            byte AktiveOptionen = (byte)(MyEnum.A | MyEnum.D);
+
+            ////////////////////////////////
+
+            string Text = "Mein Text"; // Text mit einem NULL als verstecktes ende-zeichen 
+            char Buchstabe = 'B'; // 16Bit
+            string BuchstabeAlsString = ""; // Ein Text mit der länge 0
+
+            ///////////////////////////////////
+
+            byte[] ByteArray = new byte[20]; // erstellen ein Array von 20 byte, feste länge, immer hintereinander
+            // 1. new wird aufgerufen mit der Parameter byte[20], damit weiss new wieviel arbeitspeicher benötig wird und 
+            // fordert diesen von dem Betriebssystem 
+            // ".new startet den Konstruktor von dem frisch erstellen Objekt
+            // 3. new endet mit einem return wert, welcher die arbeitsspeicheradresse des objektes enthält
+            // 4. der Rückgabewert von new wird in der Variable ByteArray gespeichert
+
+            List<byte> ListOfByte = new List<byte>(); // erstellen ein List-Objekt welches als kontainer für bytes
+            // ausgelegt wurde. dynamische länge, immer hintereinander.
+            // Fordert versteckt mehr arbeitsspeicher an als nötig damit elemente angefügt werden können
+            // Sollte der angeforderte RAM nicht reichen wird neuee Arbeitsspeicher in doppelter grösse angelegt
+            // die alten werte in den neuen bereich kopiert und der alte RAM freigegeben
+            List<byte> ListOfThousendByte = new List<byte>(1024); // legt die anfangsgrösse des RAM fest
+            // ListOfThousendByte.Count enthält die anzahl der elemente 
+            // ListOfThousendByte.Capacity enthält die grösse der Liste 
+
 
             Console.WriteLine("Hello World!");
         } // ende Main
---
+++ GitTest/GitTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MeinContainer<int> A = new MeinConteiner<int>();
+            A.Daten = 5;
+
+            MeinContainer<String> B = new MeinContainer<string>();
+            B.Daten = new Ferrari();
+
+            Ferrari Brumm = new Ferrari();
+
+            Console.WriteLine(Brumm);
+        }
+    }
+}
